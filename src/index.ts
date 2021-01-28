77ed6425748fdf8341462d3339a47d0230e40d56
Min Cai <min.cai.china@gmail.com>
2021-01-28 15:24:40 +0800

    Update index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log("Hello world.");
+import makerjs from "makerjs";
+
+const circle = {
+    type: "circle",
+    origin: [0, 0],
+    radius: 50,
+};
+
+const model = {
+    paths: {
+        circle: circle,
+        v: new makerjs.paths.Line([0, 0], [0, 100]),
+        h: new makerjs.paths.Line([0, 0], [100, 0]),
+        arc: new makerjs.paths.Arc([0, 0], 100, 0, 90),
+    },
+};
+
+function nest(model: any) {
+    const paths = model.paths;
+
+    for (const key in paths) {
+        let path = paths[key];
+
+        if (path.type === "circle") {
+            path = {
+                ...path,
+                origin: makerjs.point.add(path.origin, [10, 20]),
+            };
+        }
+
+        paths[key] = path;
+    }
+
+    model = {
+        ...model,
+        paths: paths,
+    };
+
+    return model;
+}
+
+const nestedModel = nest(model);
+
+const svg = makerjs.exporter.toSVG(model);
+const nestedSvg = makerjs.exporter.toSVG(nestedModel);
+
+console.log(svg);
+console.log(nestedSvg);
